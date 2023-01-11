45b12ec0e8a82ad59d5fa3accaf44d9d2a8ac12d
blagomail1 <41610337+blagomail1@users.noreply.github.com>
2023-01-11 18:47:23 +0200

    Update Application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -7,15 +7,37 @@ export default class Application extends EventEmitter {
     };
   }
 
-  constructor() {
-    super();
-    this.emojis = [];
-    this.banana = "🍌";
-    this.emit(Application.events.READY);
-  }
-  setEmojis(emojis) {
-    this.emojis = emojis;
-  }
+  
+  class EmojiManager {
+    constructor() {
+        this.emojis = [];
+        this.banana = "🍌";
+        this.wrapperDiv = document.getElementById('emojis');
+    }
 
-  addBananas() {}
+    setEmojis(emojis) {
+        this.emojis = emojis;
+        this.clearEmojis();
+        this.displayEmojis();
+    }
+
+    addBananas() {
+        return this.emojis.map(emoji => emoji + this.banana);
+    }
+
+    clearEmojis() {
+        this.wrapperDiv.innerHTML = '';
+    }
+
+    displayEmojis() {
+        const monkeyBananas = this.addBananas();
+        monkeyBananas.forEach(monkeyBanana => {
+            const p = document.createElement('p');
+            p.textContent = monkeyBanana;
+            this.wrapperDiv.appendChild(p);
+        });
+    }
 }
+const em = new EmojiManager();
+const monkeys = ['🐒', '🐵', '🦧'];
+document.addEventListener('DOMContentLoaded', () => em.setEmojis(monkeys));
